f570f6f9aa173f79f14161a4555afea9e6deb0fb
ella9 <ella9@DESKTOP-8OU7C66>
2021-01-23 23:01:11 +0900

    master test12

---
+++ src/myapp/HelloWorld5.java
@@ -0,0 +1,12 @@
+package myapp;
+
+public class HelloWorld5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("branch test1 Hello World5!!");
+
+
+	}
+
+}
