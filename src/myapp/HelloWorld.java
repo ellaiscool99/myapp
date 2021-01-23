20f8b578317528025f441455489aa89e255eafb5
ella9 <ella9@DESKTOP-8OU7C66>
2021-01-23 19:51:28 +0900

    patch2 update

--- src/myapp/HelloWorld.java
+++ src/myapp/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World1~~!!");
+		System.out.println("patch2 Hello World1~~!!");
 	}
 
 }
