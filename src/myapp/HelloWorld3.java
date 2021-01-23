2cdf2bd1213cc7adf099f48b6ae30cad0106eaf3
ella9 <ella9@DESKTOP-8OU7C66>
2021-01-23 20:04:26 +0900

    Merge remote-tracking branch 'origin/master' into app20210107
    Conflicts:
    	src/myapp/HelloWorld3.java

---
+++ src/myapp/HelloWorld3.java
@@ -0,0 +1,12 @@
+package myapp;
+
+public class HelloWorld3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("master patch2  Hello World3~~!!");
+
+	}
+
+}
