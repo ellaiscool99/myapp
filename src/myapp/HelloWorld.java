74c9abf16892ea6fde2900b2b5875b4f717e61cc
ella9 <ella9@DESKTOP-8OU7C66>
2021-01-23 22:53:50 +0900

    test2

--- src/myapp/HelloWorld.java
+++ src/myapp/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("master patch2 Hello World1~~!!");
+		System.out.println("test2 Hello World1~~!!");
 
 	}
 
