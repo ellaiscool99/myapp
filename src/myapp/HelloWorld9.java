a6d9ad95c98ff4969270ddc17e28e984e122b7df
ella9 <ella9@DESKTOP-8OU7C66>
2021-01-23 20:44:12 +0900

    add 9

---
+++ src/myapp/HelloWorld9.java
@@ -0,0 +1,12 @@
+package myapp;
+
+public class HelloWorld9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("add Hello World9!!");
+
+
+	}
+
+}
